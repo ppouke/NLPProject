d15eaf885c9d9d205a2632100a02ac2f29c07b99
Pauli Sakaria Pouke <32382130+ppouke@users.noreply.github.com>
2021-10-18 15:52:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from nltk import Text
 import numpy as np
 from math import log
 import os
+import re
 
 from nltk.corpus.reader.bnc import BNCCorpusReader
 from nltk.collocations import BigramAssocMeasures, BigramCollocationFinder
@@ -19,7 +20,6 @@ def mutInf(A, B, AB, sizeCorpus, span):
     # span = span of words around node word
     return log( (AB * sizeCorpus)/(A*B*span) ) / 0.30103
 
-
 bnc_reader = BNCCorpusReader(root="download\Texts", fileids=r'[A]/\w*/\w*\.xml')
 
 list_of_fileids = ['A/A0/A00.xml', 'A/A0/A01.xml', 'A/A0/A02.xml', 'A/A0/A03.xml', 'A/A0/A04.xml', 'A/A0/A05.xml']
@@ -28,18 +28,17 @@ bncwords = bnc_reader.tagged_words(fileids=list_of_fileids)
 #scored = finder.score_ngrams(bigram_measures.raw_freq)
 
 
-size_corpus = len(bncwords)
-print(size_corpus)
+
+
 
 #import testC
 path = os.path.dirname(os.path.abspath(__file__))
-files = ".*\.txt"
+files = "testCorpus.txt"
 corpus0 = PlaintextCorpusReader(path, files)
 testCorpus  = nltk.Text(corpus0.words())
 
 
-#brown remove stopwords
-
+#bnc remove stopwords
 es = set(stopwords.words('english'))
 content = [w for w in bncwords if w[0].lower() not in es]
 
@@ -48,34 +47,57 @@ content = [w for w in bncwords if w[0].lower() not in es]
 #
 # finder.apply_freq_filter(3)
 
-tagless = [x[0] for x in content]
-fd = FreqDist(tagless)
 
+def findMutualInformation(corpus, testWords, corpus_size, span):
+
+    tagless = [x[0] for x in content]
+    fd = FreqDist(tagless)
+
+    metaphorWords = {}
+    for t in testWords:
+        metaphorWords[t] = []
+
+    for word in testWords:
+        finder = BigramCollocationFinder.from_words(corpus)
+        fdfilter = lambda *w: word not in w[0]
+        tagfilter = lambda w1, w2: "VERB" not in w2[1] and "ADJ" not in w2[1] and "ADV" not in w2[1]
+        finder.apply_ngram_filter(fdfilter)
+        finder.apply_ngram_filter(tagfilter)
+
+        A = fd[word]
+        for c in finder.ngram_fd.items():
+            col = c[0][1][0]
+            B = fd[col]
+            AB = c[1]
+            mi = mutInf(A, B, AB, corpus_size, span)
+            if mi >= 3:
+                metaphorWords[word].append(col)
+
+            #mi_like score
+            #scored = finder.score_ngrams(bigram_measures.mi_like)
+            #print(scored)
+    return metaphorWords
+
+#1. Find metaphors for words in BNC
 testWords = ["woman", "use", "dream", "body"]
+size_corpus = len(bncwords)
+#mets = findMutualInformation(content, testWords, size_corpus, 4)
+#print(mets)
 
-for word in testWords:
-    finder = BigramCollocationFinder.from_words(content)
-    fdfilter = lambda *w: word not in w[0]
-    tagfilter = lambda w1, w2: "VERB" not in w2[1] and "ADJ" not in w2[1] and "ADV" not in w2[1]
+#2. test Metaphor finding w/ testCorpus
+
+headwords = {}
+f = open("testCorpus.txt", "r")
+lines = f.readlines()
+for count, line in enumerate(lines):
 
 
-    finder.apply_ngram_filter(fdfilter)
-    finder.apply_ngram_filter(tagfilter)
+    words = line.split()
+    headwordIndex = int(words[-1].split("@")[1])
+    headword = re.sub(r'[^a-zA-Z ]', '', str(count) + " : " + words[headwordIndex -1])
+    headwords[headword] = []
+    for i in [-3, -2, 0, 1]:
+        headwords[headword].append(words[headwordIndex + i])
+        
 
-
-    A = fd[word]
-
-    for c in finder.ngram_fd.items():
-        col = c[0][1][0]
-
-        B = fd[col]
-
-        AB = c[1]
-
-        mi = mutInf(A, B, AB, size_corpus, 2)
-
-        print(mi)
-
-    #mi_like score
-    #scored = finder.score_ngrams(bigram_measures.mi_like)
-    #print(scored)
+print(headwords)
